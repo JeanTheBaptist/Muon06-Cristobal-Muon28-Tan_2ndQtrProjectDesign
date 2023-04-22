5db3f55b41980ec497d22dc4eae2f0c31641516e
JeanTheBaptist <84333282+JeanTheBaptist@users.noreply.github.com>
2023-04-22 23:59:58 +0800

    Update Weapon.java

--- src/q2classmodels/Weapon.java
+++ src/q2classmodels/Weapon.java
@@ -12,13 +12,11 @@ public class Weapon extends Item{
     private int ammo, ammoMax, baseDamage;
     private boolean isAoe;
     private String damageType;
-    public Weapon(String name, int price, int worth, Ship owner, int tier){
-        super(name, "weapon", price, worth, owner);
-        this.tier = tier;
+    public Weapon(String name, int price, int worth, int staminaCost, Ship owner){
+        super(name, "weapon", price, worth, staminaCost, owner);
         String[] damageTypes = {"physical", "explosive", "magic"};
         damageType = damageTypes[(int)Math.floor(Math.random()*(3))];
-        staminaCost = (int) Math.floor(Math.random()*(3)+1);
-        baseDamage = (int)Math.floor(Math.random()*((owner.getInfluence()+11)- staminaCost+3+tier)+staminaCost+2+tier);
+        baseDamage = (int)Math.floor(Math.random()*((owner.getInfluence()+11)- staminaCost+3)+staminaCost+2);
         ammoMax = (int)Math.floor((Math.random()*(10+staminaCost+1)+staminaCost)*(1+owner.getInfluence()/10));
         ammo =ammoMax;
         if(Math.random()>0.5){
@@ -29,19 +27,14 @@ public class Weapon extends Item{
         }
     }
     public Weapon(String name, int price, int worth, int staminaCost, Ship owner, int ammoMax, int baseDamage){
-        super(name, "weapon", price, worth, owner);
-        this.tier = 0;
+        super(name, "weapon", price, worth, staminaCost, owner);
         this.price = price;
         this.worth = worth;
-        this.staminaCost = staminaCost;
         this.baseDamage = baseDamage;
         this.ammoMax = ammoMax;
         this.isAoe = false;
     }
     
-    public int getTier(){
-        return tier;
-    }
     public int getAmmo(){
         return ammo;
     }
